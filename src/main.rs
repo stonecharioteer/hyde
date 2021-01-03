abdc9d85d8eba6b8fd020b705866c2a4421f1b64
Vinay Keerthi <11478411+stonecharioteer@users.noreply.github.com>
2021-01-03 16:03:18 +0530

    feat: add placeholder CLI commands
    * add subcommands
    * add cheat subcommand action
    * add docstrings

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
 // CLI definition
+use std::fs::File;
+use std::io;
+use std::io::Write;
+use std::path::{Path, PathBuf};
+
 use structopt::StructOpt;
 
 /// Create and Manage Markdown files and their header content.
@@ -29,13 +34,95 @@ use structopt::StructOpt;
 /// and in the `%APPDIR%` on Windows.
 ///
 /// Check out the documentation on https://docs.rs/hyde
-#[derive(StructOpt)]
-struct Cli {
-    /// pattern to look for
-    pattern: String,
-    /// the path to a file to read,
-    #[structopt(parse(from_os_str))]
-    path: std::path::PathBuf,
+/// Hyde has a https://github.com/cheat/cheat compliant cheatsheet embedded into it.
+/// Use `hyde cheat` to output the cheat file to STDOUT.
+/// You can store it in your personal cheat folder,
+///
+/// which you can find using `cheat -d | grep ^personal`
+///
+#[allow(dead_code)]
+#[derive(Debug, StructOpt)]
+#[structopt(name = "hyde")]
+struct HydeCli {
+    /// Config file name, without the `.toml` extension.
+    /// This file will be stored in a location controlled
+    /// by the platform: either in `~/.config/hyde/` (Linux and OSX), or,
+    /// in `%APPDIR%` in Windows.
+    #[structopt(short, long, default_value = "default")]
+    name: String,
+    /// verbosity, chain multiple to increase the level.
+    #[structopt(short, parse(from_occurrences))]
+    verbose: u8,
+    #[structopt(subcommand)]
+    cmd: BaseSubCommands,
+    // pattern to look for
+    // pattern: String,
+    // the path to a file to read,
+    // #[structopt(parse(from_os_str))]
+    // path: std::path::PathBuf,
+}
+
+/// Subcommands for Cli.
+#[allow(dead_code)]
+#[derive(Debug, StructOpt)]
+enum BaseSubCommands {
+    /// Command to configure hyde.
+    ///
+    Config {
+        #[structopt(subcommand)]
+        cmd: ConfigSubCommands,
+    },
+    /// Create a new markdown post.
+    Create {
+        /// Configure the frontmatter.
+        /// This will override the default frontmatter you've configured in
+        /// the configuration file.
+        ///
+        /// The format for this is a string.
+        #[structopt(short, long, multiple = true)]
+        frontmatter: Vec<String>,
+        /// post title
+        /// This is the actual title of the post.
+        /// The filename will be `title.to_lowercase() + ".md"`
+        title: String,
+    },
+    /// Publish a markdown post to the default publish directory.
+    Publish {},
+    /// Outputs a https://github.com/cheat/cheat compliant cheatsheet
+    /// for `hyde`. If you specify the output location, using `-o`,
+    /// it will output to that file, or else it outputs to STDOUT.
+    ///
+    Cheat {
+        /// Output to file.
+        /// If omitted, hyde will output to STDOUT.
+        /// You may use this will `less`, or manually redirect to a file.
+        #[structopt(short, long)]
+        output: Option<Option<String>>,
+    },
+}
+
+/// Config Subcommands
+#[allow(dead_code)]
+#[derive(Debug, StructOpt)]
+enum ConfigSubCommands {
+    /// command to create a config file.
+    /// Use as `hyde config create` or `hyde -n blog config create`
+    ///
+    Create {},
+    /// command to set a config value in the config file.
+    /// Use as `hyde config set published.drafts=false`
+    Set {
+        input: String,
+    },
+    Get {
+        key: String,
+    },
+    Add {
+        key: String,
+    },
+    Remove {
+        key: String,
+    },
 }
 
 /// Helper function to print the type of an object.
@@ -45,12 +132,49 @@ fn type_of<T>(_: &T) {
 }
 
 fn main() {
-    let args = Cli::from_args();
+    let args = HydeCli::from_args();
     process_cli(args);
 }
 
 /// Process the parameters given from the CLI input.
-fn process_cli(args: Cli) {
-    type_of(&args);
-    println!("path: {:?}, pattern: {}", args.path, args.pattern);
+fn process_cli(args: HydeCli) {
+    match args.cmd {
+        BaseSubCommands::Create { frontmatter, title } => {
+            create_post(&title, &frontmatter);
+        }
+        BaseSubCommands::Cheat { output } => match output {
+            Some(file_path) => cheat(&file_path),
+            _ => {
+                cheat(&None);
+            }
+        },
+        _ => {
+            println!("oops, {:?} was unaccounted", &args.cmd);
+        }
+    };
+}
+
+fn create_post(title: &String, frontmatter: &Vec<String>) {
+    println!(
+        "You're creating something: frontmatter: {:?}, title: {}",
+        frontmatter, title
+    );
+}
+
+fn cheat(output: &Option<String>) {
+    let mut out_writer = match output {
+        Some(x) => {
+            let path = Path::new(x);
+            println!("Writing to a file {:?}", &path);
+            Box::new(File::create(&path).unwrap()) as Box<dyn Write>
+        }
+        None => {
+            println!("No output path, writing to stdout.");
+            Box::new(io::stdout()) as Box<dyn Write>
+        }
+    };
+    match out_writer.write("Woo!".as_bytes()) {
+        Ok(_) => (),
+        Err(e) => panic!("Error! {:?}", e),
+    }
 }
